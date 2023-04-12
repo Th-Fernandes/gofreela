be009ede7219343345315f177ec3c49695c0c63c
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-04-12 13:59:25 -0300

    feat: sessão de descrição de vaga

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Container } from "./components/container";
 import { Header } from "./components/header";
 
 export default function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+
+      <Container as="section">
+        <h1 className="text-xl md:text-2xl font-bold">Implementar css para um sistema - tailwind com react</h1>
+        <p>Veja os candidatos que aplicaram para a sua vaga</p>
+      </Container>
+    </>
   )
 }
