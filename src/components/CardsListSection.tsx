3cdb5ec32e388925ca7288f1deaaf8d91e38070b
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-04-12 15:39:26 -0300

    feat: renderizando dinamicamente os usuarios

--- src/components/CardsListSection.tsx
+++ src/components/CardsListSection.tsx
@@ -1,13 +1,18 @@
+import { useUsers } from "../hooks/useUsers";
 import { Container } from "./container";
 import { UserInfoCard } from "./userInfoCard";
 
 export function CardsListSection() {
+  const { users } = useUsers({amount: 28})
+
   return (
-    <Container as="section" >
+    <Container as="section" className="mb-8">
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">  
-        <UserInfoCard />
-        <UserInfoCard />
-        <UserInfoCard />
+        {
+          users?.map((user) => (
+            <UserInfoCard key={user.login.uuid}/>
+          ))
+        }
       </ul>
     </Container>
   )
